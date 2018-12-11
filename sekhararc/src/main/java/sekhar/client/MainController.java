690c25054d6a15debe66efddeb0c7f6d22bb8278
ravi <sekhar4091@gmail.com>
2018-12-11 18:53:44 +0530

    all

---
+++ sekhararc/src/main/java/sekhar/client/MainController.java
@@ -0,0 +1,36 @@
+package sekhar.client;
+
+import org.cruxframework.crux.core.client.controller.Controller;
+import org.cruxframework.crux.core.client.controller.Expose;
+import org.cruxframework.crux.core.client.ioc.Inject;
+import org.cruxframework.crux.core.client.screen.views.BindView;
+import org.cruxframework.crux.core.client.screen.views.WidgetAccessor;
+import org.cruxframework.crux.smartfaces.client.dialog.MessageBox;
+import org.cruxframework.crux.smartfaces.client.dialog.MessageBox.MessageType;
+
+import com.google.gwt.user.client.ui.TextBox;
+
+@Controller("mainController")
+public class MainController 
+{
+    @Inject
+    public MainView screen;
+    
+    @Expose
+    public void onLoad()
+    {
+        //do something
+    }
+    
+    @Expose   
+    public void sayOk()
+    {
+    	MessageBox.show("OK : " + screen.nameTextBox().getText() , MessageType.INFO);
+    }  
+    
+    @BindView("main")
+    public static interface MainView extends WidgetAccessor
+    {
+        TextBox nameTextBox();
+    }
+}
